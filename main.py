9e268f8f6634efabd67490757cb728372eae44c2
Chirag Prajapati <86082806+cprajapa@users.noreply.github.com>
2022-11-16 20:29:38 +0000

    add forward and back

--- main.py
+++ main.py
@@ -8,64 +8,98 @@ golf = Golf()
 
 
 def golfGame(distance):
-    golf.drawHole(distance)
+	
+	golf.drawHole(distance)
 
-    if distance >= 100 and distance <= 250:
+	if distance >= 100 and distance <= 250:
 
-        par = 3
+			par = 3
 
-        golf.drawText("Hole 1: Par 3, " + str(distance) + " yards")
+			golf.drawText("Hole 1: Par 3, " + str(distance) + " yards")
 
-    elif distance >= 251 and distance <= 450:
+	elif distance >= 251 and distance <= 450:
 
-        par = 4
+			par = 4
 
-        golf.drawText("Hole 1: Par 4, " + str(distance) + " yards")
+			golf.drawText("Hole 1: Par 4, " + str(distance) + " yards")
 
-    elif distance >= 451 and distance <= 650:
+	elif distance >= 451 and distance <= 650:
 
-        par = 5
+			par = 5
 
-        golf.drawText("Hole 1: Par 5, " + str(distance) + " yards")
+			golf.drawText("Hole 1: Par 5, " + str(distance) + " yards")
 
-    totalShots = 0
-    while distance > 10:
+	totalShots = 0
+	while distance > 10:
 
-        wedge = "1"
-        iron = "2"
-        driver = "3"
+			wedge = "1"
+			iron = "2"
+			driver = "3"
 
-        print(wedge + ") Wedge")
-        print(iron + ") Iron")
-        print(driver + ") Driver")
-        print()
+			print(wedge + ") Wedge")
+			print(iron + ") Iron")
+			print(driver + ") Driver")
+			print()
 
-        club = input("Select club: ")
+			club = input("Select club: ")
+			
+			print()	
 
-        if club == wedge:  # lower power, higher angle/loft
-            powerLevel = random.randint(11, 20)
-            angle = random.randint(60, 80)
-        elif club == iron:  # medium power, medium angle/loft
-            powerLevel = random.randint(21, 30)
-            angle = random.randint(45, 59)
-        elif club == driver:  # higher power, lower angle/loft
-            powerLevel = random.randint(31, 40)
-            angle = random.randint(30, 44)
-        else:
-            print("Select the correct club:")
+			forward = "1"
+			back = "2"
+		
+			direction = input("Type 1 to go forward or 2 to go back: ")
 
-        shot = golf.shootBall(powerLevel, angle)
-        distance -= shot
-        totalShots += 1
+			if direction == forward:
+				
+				if club == wedge:  # lower power, higher angle/loft
+						powerLevel = random.randint(11, 20)
+						angle = random.randint(60, 80)
+				elif club == iron:  # medium power, medium angle/loft
+						powerLevel = random.randint(21, 30)
+						angle = random.randint(45, 59)
+				elif club == driver:  # higher power, lower angle/loft
+						powerLevel = random.randint(31, 40)
+						angle = random.randint(30, 44)
+				else:
+						print("Select the correct club:")
+					
+			if direction == back:
+				
+				if club == wedge:  # lower power, higher angle/loft
+						powerLevel = random.randint(-20, -11)
+						angle = random.randint(60, 80)
+				elif club == iron:  # medium power, medium angle/loft
+						powerLevel = random.randint(-30, -21)
+						angle = random.randint(45, 59)
+				elif club == driver:  # higher power, lower angle/loft
+						powerLevel = random.randint(-40, -31)
+						angle = random.randint(30, 44)
+				else:
+						print("Select the correct club:")
 
-        print("You shot the ball " + str(round(shot, 1)) + " yards")
-        print("You have " + str(round(distance, 1)) + " yards remaining")
+			else:
+					print("Select the correct direction:")
+				
+			shot = golf.shootBall(powerLevel, angle)
+			distance -= shot
+			totalShots += 1
+		
+			print()
+		
+			print("You shot the ball " + str(round(shot, 1)) + " yards")
+			print()
+		
+			print("You have " + str(round(distance, 1)) + " yards remaining")
+			print()
 
-    print("Finished! You got the ball in the hole")
+	print("Finished! You got the ball in the hole")
+	print()
 
-    print("Your score is " + str(totalShots - par))
+	print("Your score is " + str(totalShots - par))
+	print()
 
-    golf.clearScreen()
+	golf.clearScreen()
 
 
 d1 = random.randint(100, 650)
@@ -73,6 +107,6 @@ d2 = random.randint(100, 650)
 d3 = random.randint(100, 650)
 
 for i in range(d1, d2, d3):
-    golfGame(d1)
-    golfGame(d2)
-    golfGame(d3)
+	golfGame(d1)
+	golfGame(d2)
+	golfGame(d3)
