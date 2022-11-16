bb7aa06c7f42c037ed45eec3ffb4caf01e721439
Chirag Prajapati <86082806+cprajapa@users.noreply.github.com>
2022-11-16 03:31:53 +0000

    final

--- main.py
+++ main.py
@@ -1,74 +1,78 @@
 # See the README file for how to use the golf module
 
 from Golf import Golf
+import random
 
 golf = Golf()
-import random
 
 
 def golfGame(distance):
-    if distance >= 100 and distance <= 250:
 
-        par = 3
+	golf.drawHole(distance)
 
-        golf.drawText("Hole 1: Par 3, " + str(distance) + " yards")
+	if distance >= 100 and distance <= 250:
 
-    elif distance >= 251 and distance <= 450:
+			par = 3
 
-        par = 4
+			golf.drawText("Hole 1: Par 3, " + str(distance) + " yards")
 
-        golf.drawText("Hole 1: Par 4, " + str(distance) + " yards")
+	elif distance >= 251 and distance <= 450:
 
-    elif distance >= 451 and distance <= 650:
+			par = 4
 
-        par = 5
+			golf.drawText("Hole 1: Par 4, " + str(distance) + " yards")
 
-        golf.drawText("Hole 1: Par 5, " + str(distance) + " yards")
+	elif distance >= 451 and distance <= 650:
 
-    totalShots = 0
-    while distance > 10:
+			par = 5
 
-        wedge = "1"
-        iron = "2"
-        driver = "3"
+			golf.drawText("Hole 1: Par 5, " + str(distance) + " yards")
 
-        print(wedge + ") Wedge")
-        print(iron + ") Iron")
-        print(driver + ") Driver")
-        print()
+	totalShots = 0
+	while distance > 10:
 
-        club = int(input("Select club: "))
+			wedge = "1"
+			iron = "2"
+			driver = "3"
 
-        if club == wedge:  # lower power, higher angle/loft
-            powerLevel = random.randit(11, 20)
-            angle = random.randint(60, 80)
-        elif club == iron:  # medium power, medium angle/loft
-            powerLevel = random.randit(21, 30)
-            angle = random.randint(45, 59)
-        elif club == driver:  # higher power, lower angle/loft
-            powerLevel = random.randit(31, 40)
-            angle = random.randint(30, 44)
-        else:
-            print("Select the correct club:")
+			print(wedge + ") Wedge")
+			print(iron + ") Iron")
+			print(driver + ") Driver")
+			print()
 
-        shot = golf.shootBall(powerLevel, angle)
-        distance -= shot
-        totalShots += 1
+			club = input("Select club: ")
 
-        print("You shot the ball " + str(round(shot, 1)) + " yards")
-        print("You have " + str(round(distance, 1)) + " yards remaining")
+			if club == wedge:  # lower power, higher angle/loft
+					powerLevel = random.randint(11, 20)
+					angle = random.randint(60, 80)
+			elif club == iron:  # medium power, medium angle/loft
+					powerLevel = random.randint(21, 30)
+					angle = random.randint(45, 59)
+			elif club == driver:  # higher power, lower angle/loft
+					powerLevel = random.randint(31, 40)
+					angle = random.randint(30, 44)
+			else:
+					print("Select the correct club:")
 
-    print("Finished! You got the ball in the hole")
+			shot = golf.shootBall(powerLevel, angle)
+			distance -= shot
+			totalShots += 1
 
-    print("Your score is " + str(totalShots - par))
+			print("You shot the ball " + str(round(shot, 1)) + " yards")
+			print("You have " + str(round(distance, 1)) + " yards remaining")
+
+	print("Finished! You got the ball in the hole")
+
+	print("Your score is " + str(totalShots - par))
+
+	golf.clearScreen()
 
 
 d1 = random.randint(100, 650)
 d2 = random.randint(100, 650)
 d3 = random.randint(100, 650)
-golf.drawHole(d1)
-
-distance = (d1, d2, d3)
 
 for i in range(d1, d2, d3):
-    golfGame(i)
+	golfGame(d1)
+	golfGame(d2)
+	golfGame(d3)
